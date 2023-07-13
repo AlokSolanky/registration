a8a1dc4c5c7677d178c961a1c48027fe863fed98
AlokSolanky <aloksolanky1@gmail.com>
2023-07-14 04:39:00 +0530

    12th task is also done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const frm = document.getElementById('form');
 class User{
-    constructor(mail,password)
+    constructor(name,mail,phone)
     {
+        this.name = name;
         this.mail =mail;
-        this.password = password;
+        this.phone = phone;
     }
 
     converToString()
@@ -11,18 +12,32 @@ class User{
         return JSON.stringify(this);
     }
 }
+
+
 frm.addEventListener('submit',(e)=>
 {
     e.preventDefault();
 
+    let name = document.getElementById('name').value;
     let mail = document.getElementById('mail').value;
-    let passwd = document.getElementById('password').value;
+    let phone = document.getElementById('phone').value;
 
-    const user = new User(mail,passwd);
+    const user = new User(name,mail,phone);
     
     localStorage.setItem(mail, user.converToString());
     alert("data submitted successfully");
     let formtedObj = JSON.parse(localStorage.getItem(mail));
     console.log(`email is ${formtedObj.mail} and password is ${formtedObj.password}`);
+
+    
+    let newLi = document.createElement('li');
+    newLi.setAttribute('class','list-group-item');
+    let textNode = document.createTextNode(`Name is ${name} Email is ${mail} phone number is ${phone}`);
+
+    newLi.appendChild(textNode);
+
+    const cont = document.querySelector('.li_container');
+    cont.appendChild(newLi);
 })
+
 
