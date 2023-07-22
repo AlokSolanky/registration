6789806ad99f07799b26164147732f8b2c032d34
AlokSolanky <aloksolanky1@gmail.com>
2023-07-22 22:31:32 +0530

    final project 1 sharpener

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+expenses = Object.keys(localStorage);
+for (expense of expenses) {
+  let newLi = document.createElement("li");
+  newLi.setAttribute("class", "list-group-item");
+
+  expense = JSON.parse(localStorage.getItem(expense))
+
+  let amountTextNode = document.createTextNode(expense.amount + "  ");
+  let descriptionTextNode = document.createTextNode(expense.description + "  ");
+  let typeTextNode = document.createTextNode(expense.type);
+
+  newLi.appendChild(descriptionTextNode);
+  newLi.appendChild(amountTextNode);
+  newLi.appendChild(typeTextNode);
+
+  let edit_btn = document.createElement("button");
+  edit_btn.setAttribute("class", "btn btn-sm btn-primary float-right edit");
+  edit_btn.textContent = "+";
+
+  newLi.appendChild(edit_btn);
+
+  let del_btn = document.createElement("button");
+  del_btn.setAttribute("class", "btn btn-sm btn-danger float-right delete");
+  del_btn.textContent = "X";
+
+  newLi.appendChild(del_btn);
+
+  const cont = document.querySelector(".li_container");
+  cont.appendChild(newLi);
+}
+
+
+
+
+
 // selecting form to submit
 const frm = document.getElementById("form");
 class User {
@@ -7,46 +42,47 @@ class User {
     this.type = type;
   }
 
-//   to convert the user object into string 
+  //   to convert the user object into string
   converToString() {
     return JSON.stringify(this);
   }
 }
 
-// listening if submit button is clicked 
+// listening if submit button is clicked
 frm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-//   fetching the details of form if submitted 
+  //   fetching the details of form if submitted
   let amount = document.getElementById("amount").value;
   let description = document.getElementById("description").value;
   let type = document.getElementById("type").value;
 
-//   making a new user out of this data 
+  //   making a new user out of this data
   const user = new User(amount, description, type);
 
-// checking if the mail is already registered or not 
+  // checking if the mail is already registered or not
   if (localStorage.getItem(description) === null) {
-
-    // if there is no such mail then create that mail in local storage 
+    // if there is no such mail then create that mail in local storage
     localStorage.setItem(description, user.converToString());
     let formtedObj = JSON.parse(localStorage.getItem(description));
 
-    // creating a new list to display the submitted data 
+    // creating a new list to display the submitted data
     let newLi = document.createElement("li");
     newLi.setAttribute("class", "list-group-item");
 
-    // creating 3 text nodes to store name mail and phone 
+    // creating 3 text nodes to store name mail and phone
     let amountTextNode = document.createTextNode(formtedObj.amount + "  ");
-    let descriptionTextNode = document.createTextNode(formtedObj.description + "  ");
+    let descriptionTextNode = document.createTextNode(
+      formtedObj.description + "  "
+    );
     let typeTextNode = document.createTextNode(formtedObj.type);
 
-    // appending them into list 
+    // appending them into list
     newLi.appendChild(descriptionTextNode);
     newLi.appendChild(amountTextNode);
     newLi.appendChild(typeTextNode);
 
-    // creating edit button to edit the data 
+    // creating edit button to edit the data
 
     let edit_btn = document.createElement("button");
     edit_btn.setAttribute("class", "btn btn-sm btn-primary float-right edit");
@@ -54,48 +90,44 @@ frm.addEventListener("submit", (e) => {
 
     newLi.appendChild(edit_btn);
 
-    // creating delete button to delete the data 
+    // creating delete button to delete the data
     let del_btn = document.createElement("button");
     del_btn.setAttribute("class", "btn btn-sm btn-danger float-right delete");
     del_btn.textContent = "X";
 
     newLi.appendChild(del_btn);
 
-    // now appending the list into li container 
+    // now appending the list into li container
     const cont = document.querySelector(".li_container");
     cont.appendChild(newLi);
   } else {
-    // if there is already a user registered with that mail then alert 
+    // if there is already a user registered with that mail then alert
     alert("Expense already Present");
   }
 });
 
-// listening on li container to handle the event on delete button 
+// listening on li container to handle the event on delete button
 
 let del_btn_space = document.querySelector(".li_container");
 del_btn_space.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
-
     // if clicked on delete button inside that li container
-    // then search that mail in local storage and remove it from there and from li container 
+    // then search that mail in local storage and remove it from there and from li container
     let searchDescription = e.target.parentElement.firstChild.textContent;
     localStorage.removeItem(searchDescription.trim());
     del_btn_space.removeChild(e.target.parentElement);
-  }
-  else if (e.target.classList.contains("edit")) {
-
-    // if clicked on edit button inside that li container 
-    // also search mail in local storage and if present then delete it from there 
+  } else if (e.target.classList.contains("edit")) {
+    // if clicked on edit button inside that li container
+    // also search mail in local storage and if present then delete it from there
     let searchDescription = e.target.parentElement.firstChild.textContent;
     searchDescription = searchDescription.trim();
     let obj = JSON.parse(localStorage.getItem(searchDescription));
     localStorage.removeItem(searchDescription);
     del_btn_space.removeChild(e.target.parentElement);
 
-    // also set the value in form to let the user edit his data 
+    // also set the value in form to let the user edit his data
     document.getElementById("amount").value = obj.amount;
     document.getElementById("description").value = obj.description;
     document.getElementById("type").value = obj.type;
-
   }
 });
